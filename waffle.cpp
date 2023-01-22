ab102b4710aed1e763f2835ca9a6ead9b1857856
uemuraj <uemura.junya@gmail.com>
2023-01-22 14:50:00 +0900

    プロジェクト ファイルを追加します。

---
+++ waffle.cpp
@@ -0,0 +1,17 @@
+﻿//
+// Windows Update Agent API で Windows Update のチェック・ダウンロード・インストールをしてみる：
+// 
+// https://learn.microsoft.com/ja-jp/windows/win32/api/_wua/
+// https://ascii.jp/elem/000/004/072/4072221/
+// 
+
+#include <locale>
+#include <iostream>
+#include "waffle.h"
+
+int main()
+{
+    std::locale::global(std::locale(""));
+
+    std::cout << "Hello World!\n";
+}
